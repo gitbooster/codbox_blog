a34c462c933f2d99390d856e2dd8673aeede5f8c
gitbooster <webtheory@mail.ru>
2017-05-12 11:32:11 +0300

    Grid/list swicer

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -5,8 +5,19 @@ window.log = function(param){
 
 
 
-
+// asie link active
 $('.aside_nav li a').click(function(){   
   $('.aside_nav li a').removeClass('nav_selected');
   $(this).addClass('nav_selected');
+});
+
+// change views mod on news
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();
+    $('#products .item').removeClass('grid-group-item');
+    $('#products .item').addClass('list-groups-item');});
+    
+    $('#grid').click(function(event){event.preventDefault();
+    $('#products .item').removeClass('list-groups-item');
+    $('#products .item').addClass('grid-group-item');});
 });
